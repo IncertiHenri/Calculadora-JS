8a6acc61d8cb4d684fb1e1104f7b8ff931343704
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-03-30 11:35:29 -0300

    add - Adicionada a função subtração

--- script.js
+++ script.js
@@ -15,7 +15,29 @@ function soma(){
         alert("Algum valor é inválido, insira outro valor em números!");
     } else {
         soma = (valor1+valor2);
-        alert(soma);
+        alert("O resultado da soma é: "+ soma);
+    }
+
+}
+
+function subtracao(){
+        let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+    let subtracao = 0;
+
+    if(valor1 === "" || valor2 === ""){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    valor1 = parseFloat(document.getElementById("valor1").value);
+    valor2 = parseFloat(document.getElementById("valor2").value);
+
+    if(isNaN(valor1)||isNaN(valor2)){
+        alert("Algum valor é inválido, insira outro valor em números!");
+    } else {
+        subtracao = (valor1-valor2);
+        alert("O resultado da subtração é: " + subtracao);
     }
 
 }
