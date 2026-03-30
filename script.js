6c4ff2934f6cf359ed9626a026e52b9c846bc83e
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-03-30 11:21:43 -0300

    add - Verificação numérica

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
-function calculadora(){
+function numerosAlerta(){
     let valor1 = document.getElementById("valor1").value;
     let valor2 = document.getElementById("valor2").value;
 
-    console.log(valor2);
-    console.log(valor1);
+    if(valor1 === "" || valor2 === ""){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    valor1 = parseFloat(document.getElementById("valor1").value);
+    valor2 = parseFloat(document.getElementById("valor2").value);
+
+    if(isNaN(valor1)||isNaN(valor2)){
+        alert("Algum valor é inválido, insira outro valor em números!");
+    } else {
+        alert("Valores válidos, escolha a operação!");
+    }
+
 }
