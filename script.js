95795af541b27385e599b02936781ec2553b3375
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-03-30 11:31:05 -0300

    add - Adicionada a função soma

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function soma(){
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+    let soma = 0;
 
+    if(valor1 === "" || valor2 === ""){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    valor1 = parseFloat(document.getElementById("valor1").value);
+    valor2 = parseFloat(document.getElementById("valor2").value);
+
+    if(isNaN(valor1)||isNaN(valor2)){
+        alert("Algum valor é inválido, insira outro valor em números!");
+    } else {
+        soma = (valor1+valor2);
+        alert(soma);
+    }
 
 }
