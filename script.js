7cfbb0f202577c373f2520f513a072e1201de0ba
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-03-30 11:42:38 -0300

    add - Adicionada a função divisão

--- script.js
+++ script.js
@@ -41,3 +41,27 @@ function subtracao(){
     }
 
 }
+
+function divisao(){
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+    let divisao = 0;
+
+    if(valor1 === "" || valor2 === ""){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    valor1 = parseFloat(document.getElementById("valor1").value);
+    valor2 = parseFloat(document.getElementById("valor2").value);
+
+    if(valor1 === 0 || valor2 === 0){
+        alert("A divisão por 0 não é possível, insira outro valor dirente de 0!");
+    } else if(isNaN(valor1)||isNaN(valor2)){
+        alert("Algum valor é inválido, insira outro valor em números!");
+    } else if(!isNaN(valor1)||!isNaN(valor2)){
+        divisao = (valor1/valor2);
+        alert("O resultado da divisão é: " + divisao);
+    }
+
+}
