0c9bdfbeb8cb297d0f3a80a05e2fa08a5118c231
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-03-30 11:44:50 -0300

    add - Adicionada a função multiplicação

--- script.js
+++ script.js
@@ -65,3 +65,25 @@ function divisao(){
     }
 
 }
+
+function multiplicacao(){
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+    let multiplicacao = 0;
+
+    if(valor1 === "" || valor2 === ""){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    valor1 = parseFloat(document.getElementById("valor1").value);
+    valor2 = parseFloat(document.getElementById("valor2").value);
+
+    if(isNaN(valor1)||isNaN(valor2)){
+        alert("Algum valor é inválido, insira outro valor em números!");
+    } else {
+        multiplicacao = (valor1*valor2);
+        alert("O resultado da multiplicação é: " + multiplicacao);
+    }
+
+}
