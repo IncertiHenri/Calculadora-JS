c72cf61ad2addc60de66e3fef93c51f0ef4f75da
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-03-30 11:30:42 -0300

    rev - Remoção da função de verificação

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
+function soma(){
 
-function numerosAlerta(){
-    let valor1 = document.getElementById("valor1").value;
-    let valor2 = document.getElementById("valor2").value;
-
-    if(valor1 === "" || valor2 === ""){
-        alert("Preencha todos os campos!");
-        return;
-    }
-
-    valor1 = parseFloat(document.getElementById("valor1").value);
-    valor2 = parseFloat(document.getElementById("valor2").value);
-
-    if(isNaN(valor1)||isNaN(valor2)){
-        alert("Algum valor é inválido, insira outro valor em números!");
-    } else {
-        alert("Valores válidos, escolha a operação!");
-    }
 
 }
