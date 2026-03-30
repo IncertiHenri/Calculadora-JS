a82dd6dbd74b67d5f6aea132bdab3a7b822545b0
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-03-30 10:53:11 -0300

    add - Base da calculadora

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function calculadora(){
+    let valor1 = document.getElementById("valor1").value;
+    let valor2 = document.getElementById("valor2").value;
+
+    console.log(valor2);
+    console.log(valor1);
+}
